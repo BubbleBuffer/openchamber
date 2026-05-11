8bc62e70cd61820686d5d5e707d4ab2b99a1cdd7
BubbleBuffer <botlove282@icloud.com>
2026-05-11 15:03:20 +0200

    fix: remove incorrect sentry url option

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -64,7 +64,6 @@ export default defineConfig({
       org: process.env.SENTRY_ORG,
       project: process.env.SENTRY_PROJECT,
       authToken: process.env.SENTRY_AUTH_TOKEN,
-      url: 'https://de.sentry.io',
       release: {
         name: process.env.SENTRY_RELEASE || packageJson.version,
       },
